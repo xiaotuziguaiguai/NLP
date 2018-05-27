1a02343a624665ddf85deb68535587573f42ac04
xuxu <1726468525@qq.com>
2018-05-27 17:28:51 +0800

    Update simple_sentiment.py

--- simple_sentiment.py
+++ simple_sentiment.py
@@ -1,2 +1,8 @@
 #https://www.jianshu.com/p/06a4a8e77315
 #https://www.jianshu.com/p/58bfd9e53db0
+#https://www.jianshu.com/p/92de44f0376a
+#https://www.jianshu.com/p/57a9b6103fe5
+#https://www.jianshu.com/p/16fd6e5cc21f
+#https://www.jianshu.com/p/6eb558a8fec0
+#https://www.jianshu.com/p/881b68a3f41d
+#https://www.jianshu.com/p/cd64dc753f99
